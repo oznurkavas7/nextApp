7170f3e2d0757672403ecd3ef72637d9c098eb75
oznur <kavasznr97@gmail.com>
2023-11-27 16:17:15 +0300

    setting added, small changes,bug fixes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import SettingsIcon from '@mui/icons-material/Settings';
 import IconButton from '@mui/material/IconButton';
+import { SketchPicker } from "react-color";
 import {
   TextField, Button, Checkbox, FormControlLabel, List, ListItem, ListItemText,
-  CardContent, Card, Grid, Dialog, DialogContent, DialogActions, DialogContentText
+  CardContent, Card, Grid, Dialog, DialogContent, DialogActions, DialogContentText,
+  Modal, Box, Typography, ListItemIcon, ListItemButton, Tooltip
 } from '@mui/material';
 import { Container } from 'react-bootstrap';
 import './App.css';
 import React, { useState } from "react";
-
 
 function App() {
   const [checked, setChecked] = useState(false);
@@ -24,7 +26,22 @@ function App() {
   }
   setInterval(UpdateTime)
   const [isDelBtnDisabled, setDelBtnDisabled] = useState(true);
+  const [isEditBtnDisabled, setEditBtnDisabled] = useState(null);
   const [open, setOpen] = useState(false);
+  const [openDel, setOpenDel] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 320,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,6 +49,14 @@ function App() {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleClickOpenDel = () => {
+    setOpenDel(true);
+  };
+
+  const handleCloseDel = () => {
+    setOpenDel(false);
   };
 
   function getDate() {
@@ -50,33 +75,46 @@ function App() {
         id: id,
         task: input,
         complete: false,
+        editing: false
       },
     ]);
     setDelBtnDisabled(false);
     setInput("");
     setChecked(false);
+    setIsEditing(false);
   };
 
-  const clickhandler = (item, checked) => {
+  const clickhandler = (item, index) => {
     todoList.forEach(i => {
       if (item.id === i.id) {
-        setChecked(!checked);
-        item.complete = !checked;
+        item.complete = !item.complete;
       }
+      setEditBtnDisabled(index);
     });
   }
 
-  const deleteDo = value => {
+  const deleteDo = (value) => {
     setTodoList(oldValues => {
       setDelBtnDisabled(todoList.length === 1);
       return oldValues.filter(item => item !== value)
     })
+    setOpenDel(false);
+  }
+
+  const editDo = (item) => {
+    todoList.forEach(i => {
+      if (item.id === i.id) {
+        item.editing = true;
+      }
+    });
+
   }
 
   const deleteAll = () => {
     setTodoList([]);
     setDelBtnDisabled(true);
     setOpen(false);
+    setOpenDel(false);
   }
 
   const handleKeyDown = e => {
@@ -85,15 +123,80 @@ function App() {
     }
   }
 
+  const handleKeyDownEdit = (e, item) => {
+    if (e.key === 'Enter') {
+      todoList.forEach(i => {
+        if (item.id === i.id) {
+          item.editing = false;
+          item.task = e.target.value;
+        }
+      });
+    }
+  }
+
+  const [openColorSett, setOpenColorSett] = useState(false);
+  const [color, setColor] = useState({
+    r: "241",
+    g: "112",
+    b: "19",
+    a: "1"
+  });
+
+  const colorSetting = () => {
+    setOpenColorSett(!openColorSett);
+  }
+
+  const onCloseMethod = () => {
+    setOpenColorSett(false);
+  };
+
+  const [containerColor, setContainerColor] = useState('#CCCCFF');
+
+
+  const changeContainerColor = (color) => {
+    const selectColor = color.hex;
+    setContainerColor(selectColor);
+    setColor({ ...color.rgb });
+  }
+
   return (
-    <Container>
+    <Container style={{ backgroundColor: containerColor, padding: '2rem', borderRadius: "20px 20px 20px 20px" }}>
       <div style={{ padding: 30 }}>
         <Grid container spacing={10} justify="center">
           <Grid item xs={12} display={{ md: 12 }}>
             <Card>
               <CardContent>
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <Tooltip title="Change Color">
+                    <IconButton aria-label="edit" onClick={colorSetting}>
+                      <SettingsIcon style={{ fill: "gray" }} />
+                      <Modal
+                        open={openColorSett}
+                        onClose={onCloseMethod}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                      >
+                        <Box sx={style}>
+                          <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Background Change Color
+                          </Typography>
+                          <Typography id="modal-modal-description" sx={{ mt: 2 }} component={'span'} variant={'body2'}>
+                            {openColorSett ? (
+                              <div>
+                                <div onClick={onCloseMethod} />
+                                <SketchPicker color={color} onChange={changeContainerColor} />
+                              </div>
+                            ) : null}
+                          </Typography>
+                        </Box>
+                      </Modal>
+                    </IconButton>
+                  </Tooltip>
+
+                </div>
                 <div>
                   <p>Today's Date:  <b>{currentDate} {ctime}</b></p>
+
                 </div>
                 <div style={{
                   display: 'flex',
@@ -111,7 +214,7 @@ function App() {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={12} display={{ md: todoList.length !== 0 ? 12 : "none" }}
+          <Grid item xs={12} style={{ display: todoList.length !== 0 ? "inline" : "none" }}
           >
             <div style={{ display: "flex" }}>
               <Button style={{ marginTop: 15, marginLeft: "auto" }} disabled={isDelBtnDisabled} onClick={handleClickOpen} variant="contained">Delete All</Button>
@@ -124,7 +227,7 @@ function App() {
             >
               <DialogContent>
                 <DialogContentText id="alert-dialog-description">
-                  Tüm to-do listesi silinsin mi?
+                  Do you want to delete the list?
                 </DialogContentText>
               </DialogContent>
               <DialogActions>
@@ -138,19 +241,39 @@ function App() {
               <CardContent
               ><div>
                   <List>
-                    {todoList.map((item) => (
+                    {todoList.map((item, index) => (
                       <ListItem
-                        key={item.id}
+                        key={index}
                       >
-                        <ListItemText
-                          primary={`${item.task}`} style={{ textDecorationLine: item.complete ? "line-through" : "none" }} />
-                        <IconButton aria-label="edit">
-                          <EditIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete" onClick={() => deleteDo(item)}>
-                          <DeleteIcon />
-                        </IconButton>
-                        <FormControlLabel control={<Checkbox />} label="Completed" onChange={() => clickhandler(item, checked)} value={checked} />
+                        <ListItemButton role={undefined} dense>
+                          <ListItemIcon>
+                            <Tooltip title="Complete Note">
+                              <Checkbox
+                                edge="start"
+                                onChange={() => clickhandler(item, index)} value={checked}
+                                tabIndex={-1}
+                                disableRipple
+                              />
+                            </Tooltip>
+                          </ListItemIcon>
+                          {item.editing ? (
+                            <TextField style={{ display: 'flex', width: "calc(100%)" }} defaultValue={item.task}
+                              id="outlined-basic" variant="outlined" onKeyDown={(e) => handleKeyDownEdit(e, item)} />
+                          ) : <ListItemText
+                            primary={`${item.task}`} style={{ textDecorationLine: item.complete ? "line-through" : "none" }} />}
+                        </ListItemButton>
+
+                        <Tooltip title="Edit Note">
+                          <IconButton aria-label="edit" onClick={() => editDo(item)} disabled={isEditBtnDisabled === index && item.complete}>
+                            <EditIcon style={{ fill: "blue" }} />
+                          </IconButton>
+                        </Tooltip>
+
+                        <Tooltip title="Delete Note">
+                          <IconButton aria-label="delete" onClick={() => deleteDo(item)}>
+                            <DeleteIcon style={{ fill: "red", justifyContent: 'flex-end' }} />
+                          </IconButton>
+                        </Tooltip>
                       </ListItem>
                     ))}
                   </List>
@@ -162,15 +285,29 @@ function App() {
         </Grid>
       </div>
     </Container>
+
   )
 }
 
 export default App;
 
-/* onClick={() => editDo(item)}
-  const [isEditing, setIsEditing] = useState(false);
-<div>
-            <SketchPicker>
-          </SketchPicker>
-            </div>
+/*
+     <Dialog
+                            open={openDel}
+                            onClose={handleCloseDel}
+                            aria-labelledby="alert-dialog-title"
+                            aria-describedby="alert-dialog-description"
+                          >
+                            <DialogContent>
+                              <DialogContentText id="alert-dialog-description">
+                                Do you want to delete the to-do?
+                              </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                              <Button onClick={handleCloseDel}>No</Button>
+                              <Button onClick={() => deleteDo(item)} autoFocus>
+                                Yes
+                              </Button>
+                            </DialogActions>
+                          </Dialog>
 */
